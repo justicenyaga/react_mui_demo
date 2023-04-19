137902d4cc17005e2d95d9b9a24f02509a975164
Justice Nyaga <justiceevansmwema@gmail.com>
2023-04-19 14:54:09 +0300

    Buttons

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,81 @@
+import { Stack, Button, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+function MuiButton() {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        {/* text - grab less ui attention, eg. for card footer, confirm button, links */}
+        <Button variant="text" href="https://google.com">
+          Text{" "}
+        </Button>
+
+        {/* contained - grab more ui attention, eg. for primary actions e.g register or login buttons */}
+        <Button variant="contained">Contained</Button>
+
+        {/* outlined - grab more ui attention, eg. for secondary actions e.g cancel or go back buttons */}
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="contained" color="inherit">
+          Inherit
+        </Button>
+        <Button variant="contained" color="primary" disabled>
+          Disabled
+        </Button>
+      </Stack>
+
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("Clicked")}
+        >
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+
+        {/* icon button are more subtle, eg. for navigation bar where you want to keep the ui clean and simple */}
+        <IconButton aria-label="send" color="success" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default MuiButton;
