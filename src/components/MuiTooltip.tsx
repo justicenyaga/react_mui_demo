909373babf5dda9cf6671f137aa7ec0bdd4bf42d
Justice Nyaga <justiceevansmwema@gmail.com>
2023-04-21 18:27:44 +0300

    Tooltip

---
+++ src/components/MuiTooltip.tsx
@@ -0,0 +1,20 @@
+import { Tooltip, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+function MuiTooltip() {
+  return (
+    <Tooltip
+      title="Delete"
+      placement="right"
+      arrow
+      enterDelay={500}
+      leaveDelay={200}
+    >
+      <IconButton>
+        <DeleteIcon />
+      </IconButton>
+    </Tooltip>
+  );
+}
+
+export default MuiTooltip;
