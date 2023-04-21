f55bf3f34fc580e63034f8323a505c7c8e85705f
Justice Nyaga <justiceevansmwema@gmail.com>
2023-04-22 02:21:10 +0300

    Masonry

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,8 @@ import "./App.css";
 // import MuiPicker from "./components/MuiPicker";
 // import MuiDateRangePicker from "./components/MuiDateRangePicker";
 // import MuiTabs from "./components/MuiTabs";
-import MuiTimeline from "./components/MuiTimeline";
+// import MuiTimeline from "./components/MuiTimeline";
+import MuiMasonry from "./components/MuiMasonry";
 
 function App() {
   return (
@@ -75,7 +76,8 @@ function App() {
         {/* <MuiPicker /> */}
         {/* <MuiDateRangePicker /> */}
         {/* <MuiTabs /> */}
-        <MuiTimeline />
+        {/* <MuiTimeline /> */}
+        <MuiMasonry />
       </div>
     </LocalizationProvider>
   );
