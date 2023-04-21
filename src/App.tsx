91a15a4cd146238ba43ea30401ff0c3f56350fee
Justice Nyaga <justiceevansmwema@gmail.com>
2023-04-21 16:54:37 +0300

    BottomNavigation

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ import "./App.css";
 // import MuiLink from "./components/MuiLink";
 // import MuiBreadcrumbs from "./components/MuiBreadcrumbs";
 // import MuiDrawer from "./components/MuiDrawer";
-import MuiSpeedDial from "./components/MuiSpeedDial";
+// import MuiSpeedDial from "./components/MuiSpeedDial";
+import MuiBottonNavigation from "./components/MuiBottonNavigation";
 
 function App() {
   return (
@@ -38,7 +39,8 @@ function App() {
       {/* <MuiLink /> */}
       {/* <MuiBreadcrumbs /> */}
       {/* <MuiDrawer /> */}
-      <MuiSpeedDial />
+      {/* <MuiSpeedDial /> */}
+      <MuiBottonNavigation />
     </div>
   );
 }
